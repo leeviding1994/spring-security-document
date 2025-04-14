e6b34733c20374f3d442ff7d426bb278426e9d31
LeeviDing <noah.ding@sunpuretech.com>
2025-04-14 15:41:07 +0800

    Initial commit

---
+++ src/main/java/com/dingli/spring_security_document/SpringSecurityDocumentApplication.java
@@ -0,0 +1,12 @@
+package com.dingli.spring_security_document;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDocumentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dingli/spring_security_document/SpringSecurityDocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dingli.spring_security_document;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDocumentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
